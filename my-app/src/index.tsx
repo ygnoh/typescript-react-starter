093da079b33ac20fe0878f5911f496881a3989b2
yonggoonoh <nohyg90@gmail.com>
2018-06-07 00:52:29 +0900

    add a description comment for type assertion

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -6,6 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Hello name="TypeScript" enthusiasmLevel={10} />,
+  // the below line is called "type assertion"
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
