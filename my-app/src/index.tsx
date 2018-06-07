e08369ada2a584da069f8f5ad4aa664ee6fb2b0f
yonggoonoh <nohyg90@gmail.com>
2018-06-08 00:49:53 +0900

    create store

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,6 +3,15 @@ import * as ReactDOM from 'react-dom';
 import Hello from './components/Hello';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore } from 'redux';
+import { enthusiasm } from "./reducers";
+import { StoreState } from './types';
+
+const store = createStore<StoreState>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript'
+});
+
 ReactDOM.render(
   <Hello name="TypeScript" enthusiasmLevel={10} />,
   // the below line is called "type assertion"
